33d29745d05a51d3dff84d6dab54c35174eba2b6
GlauberX86 <glauber800br@gmail.com>
2025-12-05 09:34:47 -0300

    Joke

--- Worker.cs
+++ Worker.cs
@@ -1,17 +1,39 @@
-namespace AssetManager
+namespace App.WindowsService;
+
+public sealed class WindowsBackgroundService(
+    JokeService jokeService,
+    ILogger<WindowsBackgroundService> logger) : BackgroundService
 {
-    public class Worker(ILogger<Worker> logger) : BackgroundService
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        try
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                if (logger.IsEnabled(LogLevel.Information))
-                {
-                    logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                }
-                await Task.Delay(1000, stoppingToken);
+                string joke = jokeService.GetJoke();
+                logger.LogWarning("{Joke}", joke);
+
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
             }
+        }
+        catch (OperationCanceledException)
+        {
+            // When the stopping token is canceled, for example, a call made from services.msc,
+            // we shouldn't exit with a non-zero exit code. In other words, this is expected...
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "{Message}", ex.Message);
+
+            // Terminates this process and returns an exit code to the operating system.
+            // This is required to avoid the 'BackgroundServiceExceptionBehavior', which
+            // performs one of two scenarios:
+            // 1. When set to "Ignore": will do nothing at all, errors cause zombie services.
+            // 2. When set to "StopHost": will cleanly stop the host, and log errors.
+            //
+            // In order for the Windows Service Management system to leverage configured
+            // recovery options, we need to terminate the process with a non-zero exit code.
+            Environment.Exit(1);
         }
     }
 }
