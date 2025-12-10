414f756f0b1fc7dc11ceebe45af6c0a792d123c1
GlauberX86 <glauber800br@gmail.com>
2025-12-10 09:48:23 -0300

    Named Pipe teste

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,14 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "AssetManager";
+});
 
 builder.Services.AddSingleton<ICpuMonitor, CpuMonitor>();
 builder.Services.AddSingleton<IMemoryMonitor, MemoryMonitor>();
